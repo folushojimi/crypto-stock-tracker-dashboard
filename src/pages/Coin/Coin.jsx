3e624ed0fddf7df2d57c7ef0e80f4cc86d7c2b18
folushojimi <iamrblogger@gmail.com>
2025-01-23 23:16:04 +0100

    implementing the coin page

--- src/pages/Coin/Coin.jsx
+++ src/pages/Coin/Coin.jsx
@@ -1,12 +1,30 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
+import { CoinContext } from '../../context/CoinContext'
  
 const Coin = () => {
-
 const {coinId}=useParams()
+const [  coinData, setCoinData]=useState() 
+const {currency}=useContext(CoinContext)
+
+const fetchCoinData = async ()=>{
+  const options = {method: 'GET', headers: {accept: 'application/json'}};
+
+  fetch(`https://api.coingecko.com/api/v3/coins/${coinId} `, options)
+    .then(res => res.json())
+    .then(res => setCoinData(res))
+    .catch(err => console.error(err));
+}
+
+useEffect(()=>{
+  fetchCoinData();   
+},[currency])
+ 
+console.log(coinData)
+
   return (
     <div>
-    <h1 className='text-white font-bold'>Coin</h1>
+    <h1 className='text-white font-bold'>Coin : { }</h1>
     </div>
   )
 }
